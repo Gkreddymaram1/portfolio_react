9e03fb7c3248f91fe816da933fba5ce1e02a7427
Gkreddymaram1 <maramgopalkrishnareddy@gmail.com>
2026-02-03 22:04:26 +0530

    Production errors got fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,4 @@
-// import React from 'react'
-// import './App.css'
-// import Home from './components/Home';
-// import About from './components/About';
-// import Contact from './components/Contact'
-// import {Routes,Route } from 'react-router-dom'; 
-// import Navbar from './components/Navbar';
-// import User from './components/user';
-// import Footer from './components/Footer'
-// import Skills from './components/Skills';
-// import Projects from './components/Projects';
-// import HeroSection from'./components/Herosection';
 
-
-
-// export default function App() {
-//   return (
-
-
-// <>
-
-
-// <Navbar />
-// <HeroSection/>
-
-
-// <Routes>
-
-// <Route path='/home' element={ <Home/>}/>
-// <Route path='/About' element={ <About/>}/>
-// <Route path='/Contact' element={ <Contact/>}/>
-// <Route path='/User' element={ <User/>}/>
-// <Route path='/Skills' element={ <Skills/>}/>
-//  <Route path='/Projects' element={ <Projects/>}/>
-//  <Route path='*' element={<h1>Not Found data</h1>}/>
-//    </Routes> 
-
-
-// <Footer/>
-// </>
-
-
-
-
-//   )
-// }
 
 
 
@@ -55,17 +10,17 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Layout components
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/Herosection";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.jsx";
+import HeroSection from "./components/Herosection.jsx";
+import Footer from "./components/Footer.jsx";
 
 // Pages / Sections
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import User from "./components/User";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import User from "./components/User.jsx";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
 
 export default function App() {
   return (
