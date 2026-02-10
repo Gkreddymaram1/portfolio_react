2a8a6f0ddd7688aed90a4fbef8520899e2fd2e86
Gkreddymaram1 <maramgopalkrishnareddy@gmail.com>
2026-02-10 20:14:06 +0530

    "components"

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,30 +1,3 @@
-// import React from "react";
-// import "./Contact.css";
-
-// export default function Contact() {
-//   return (
-//     <div className="contact-page">
-//       <form className="contact-form">
-//         <h2>Contact Form</h2>
-
-//         <label>Name</label>
-//         <input type="text" placeholder="Enter your name" required />
-
-//         <label>Email</label>
-//         <input type="email" placeholder="Enter your email" required />
-
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "./Contact.css";
